d3d07292e8adfce8f8bd50ac4a4f03bceec7de80
hwseo0406 <hse05078@gmail.com>
2024-07-16 13:48:39 +0900

    add MyPage

--- src/pages/CustomerService.js
+++ src/pages/CustomerService.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import './CustomerService.css';
 
 const CustomerService = () => {
   return (
-    <div>
-      <h1>고객센터</h1>
-      <p>불만있으면 전화해!
-        010-4859-1234
-      </p>
+    <div className="customer-service-root">
+      <img src='/KakaoTalk_20240711_092137325.jpg' alt="가계뷰리 배경화면" className="background-image" />
+      <div className="customer-service-content">
+        <h1>고객센터</h1>
+        
+        <section className="customer-service-faq">
+          <h2>자주 묻는 질문</h2>
+          <div className="customer-service-question">
+            <h3>Q1: 새로운 계정을 어떻게 만드나요?</h3>
+            <p>A1: 홈페이지에서 "회원가입" 버튼을 클릭하고 필요한 정보를 입력하시면 됩니다.</p>
+          </div>
+          <div className="customer-service-question">
+            <h3>Q2: 비밀번호를 어떻게 재설정하나요?</h3>
+            <p>A2: 로그인 페이지에서 "비밀번호 재설정" 링크를 클릭하고 지시에 따라 진행하세요.</p>
+          </div>
+          <div className="customer-service-question">
+            <h3>Q3: 고객센터에 어떻게 연락하나요?</h3>
+            <p>A3: 아래 제공된 전화번호로 연락하시거나 kdigital@bu.ac.kr으로 이메일을 보내주세요.</p>
+          </div>
+        </section>
+        
+        <section className="customer-service-contact">
+          <h2>연락처</h2>
+          <p>다른 질문이 있으시거나 추가 도움이 필요하시면 고객센터로 연락해 주세요:</p>
+          <p><strong>전화번호:</strong> 010-6862-7407</p>
+          <p><strong>이메일:</strong> kdigital@bu.ac.kr</p>
+        </section>
+      </div>
     </div>
   );
 };
